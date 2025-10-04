368b3c17b34170eca29382dbd83866e921fce102
the-eris-net <the.eris.net@gmail.com>
2025-10-05 06:09:18 +0900

    feat:finish 21p

--- 23p-nested_context_self/src/App.jsx
+++ 23p-nested_context_self/src/App.jsx
@@ -1,53 +1,28 @@
-import { createContext,useContext } from 'react';
-import { useState } from 'react';
+import { createContext, useContext } from "react";
 
-const ThemeContext = createContext();
-const AuthContext = createContext();
+const ThemeContext = createContext("light");
 
+function Content() {
+  const theme = useContext(ThemeContext);
+  return <p>Content 현재 테마: {theme}</p>;
+}
 
-function Card({ title, content }) {
-  const { isDarkMode } = useContext(ThemeContext);
-  const { isLoggedIn } = useContext(AuthContext);
-
+function Layout() {
+  const theme = useContext(ThemeContext);
   return (
-    <div
-      style={{
-        border: `1px solid ${isDarkMode ? '#444' : '#ccc'}`,
-        borderRadius: '8px',
-        padding: '16px',
-        color: isDarkMode ? '#fff' : '#000',
-        backgroundColor: isDarkMode ? '#333' : '#fff',
-      }}  
-    >
-      <strong>{title}</strong>
-      <div style={{ margin: 8 }}>
-        {isLoggedIn ? <> {content} </> : <>로그인 후 내용을 확인하세요.</>}
-      </div>
-    </div>
+    <ThemeContext value="blue">
+      <p>Layout 현재 테마: {theme}</p>
+      <Content />
+    </ThemeContext>
   );
-} 
-          
+}
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
+  const theme = useContext(ThemeContext);
   return (
-    <ThemeContext value={{ isDarkMode }}>
-      <AuthContext value={{ isLoggedIn }}>
-        <div>
-          <button onClick={() => setIsDarkMode(!isDarkMode)}>
-            Toggle Dark Mode : {isDarkMode ? 'ON' : 'OFF'}
-          </button>
-          <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
-            {isLoggedIn ? '로그아웃' : '로그인'}
-          </button>
-          <Card
-            title="오늘 공부 체크리스트"
-            content="리액트 Context API 사용하기"
-          />
-        </div>
-      </AuthContext>  
+    <ThemeContext value="dark">
+      <p>App 현재 테마: {theme}</p>
+      <Layout />
     </ThemeContext>
   );
 }
