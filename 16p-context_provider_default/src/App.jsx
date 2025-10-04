71789a4ef35de976499347fa6dde18d097e76932
the-eris-net <the.eris.net@gmail.com>
2025-10-05 03:48:45 +0900

    feat:add

--- 16p-context_provider_default/src/App.jsx
+++ 16p-context_provider_default/src/App.jsx
@@ -1,11 +1,16 @@
-import { useState, useEffect } from 'react';
+import { createContext,useContext } from 'react';
+import { useState } from 'react';
 
-function ContentItem({ item, isDarkMode }) {
+const MyContext = createContext();
+
+function ContentItem({ item }) {
+  const context = useContext(MyContext);
+
   return (
     <div
       style={{
-        backgroundColor: isDarkMode ? '#fff' : '#333',
-        color: isDarkMode ? '#000' : '#fff',
+        backgroundColor: context.isDarkMode ? '#fff' : '#333',
+        color: context.isDarkMode ? '#000' : '#fff',
         margin: 8,
         padding: 8,
         borderRadius: 8,
@@ -18,21 +23,23 @@ function ContentItem({ item, isDarkMode }) {
   );
 }
 
-function Card({ title, content, isDarkMode }) {
+function Card({ title, content }) {
+  const context = useContext(MyContext);
+
   return (
     <div
       style={{
-        border: `1px solid ${isDarkMode ? '#444' : '#ccc'}`,
+        border: `1px solid ${context.isDarkMode ? '#444' : '#ccc'}`,
         borderRadius: '8px',
         padding: '16px',
-        color: isDarkMode ? '#fff' : '#000',
-        backgroundColor: isDarkMode ? '#333' : '#fff',
+        color: context.isDarkMode ? '#fff' : '#000',
+        backgroundColor: context.isDarkMode ? '#333' : '#fff',
       }}
     >
       <strong>{title}</strong>
       <div style={{ margin: 8 }}>
         {content.map((item) => (
-          <ContentItem key={item.id} item={item} isDarkMode={isDarkMode} />
+          <ContentItem key={item.id} item={item} />
         ))}
       </div>
     </div>
@@ -41,24 +48,25 @@ function Card({ title, content, isDarkMode }) {
 
 function App() {
   const data = [
-    { id: 1, text: 'JSX 알아보기'},
+    { id: 1, text: 'JSX 알아보기' },
     { id: 2, text: '컴포넌트 스타일링 해보기' },
-    { id: 3, text: '일정 관리 앱 만들어 보기'},
+    { id: 3, text: '일정 관리 앱 만들어 보기' },
   ];
 
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   return (
-    <div>
-      <button onClick={() => setIsDarkMode(!isDarkMode)}>
-        Toggle Dark Mode : {isDarkMode ? 'ON' : 'OFF'}
-      </button>
-      <Card
-        title="오늘 공부 체크리스트"
-        content={data}
-        isDarkMode={isDarkMode}
-      />
-    </div>
+    <MyContext value={{ isDarkMode }}>
+      <div>
+        <button onClick={() => setIsDarkMode(!isDarkMode)}>
+          Toggle Dark Mode : {isDarkMode ? 'ON' : 'OFF'}
+        </button>
+        <Card
+          title="오늘 공부 체크리스트"
+          content={data}
+        />
+      </div>
+    </MyContext>
   );
 }
 
