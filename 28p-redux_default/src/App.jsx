a0d98da997acabc8edaaa053fd4a147865a3ecca
the-eris-net <the.eris.net@gmail.com>
2025-10-05 20:19:09 +0900

    feat:default redux

--- 28p-redux_default/src/App.jsx
+++ 28p-redux_default/src/App.jsx
@@ -1,72 +1,55 @@
-import { createContext,useContext } from 'react';
-import { useState } from 'react';
+import { useEffect,useState } from 'react';
+import store from './stores';
 
-const MyContext = createContext();
-
-function ContentItem({ item }) {
-  const context = useContext(MyContext);
-
-  return (
-    <div
-      style={{
-        backgroundColor: context.isDarkMode ? '#fff' : '#333',
-        color: context.isDarkMode ? '#000' : '#fff',
-        margin: 8,
-        padding: 8,
-        borderRadius: 8,
-      }}
-    >
-      <span>
-        {item.id} : {item.text}
-      </span>
-    </div>
-  );
-}
 
 function Card({ title, content }) {
-  const context = useContext(MyContext);
-
+  const {isDarkMode, isLoggedIn} = store.getState();
+  
   return (
     <div
       style={{
-        border: `1px solid ${context.isDarkMode ? '#444' : '#ccc'}`,
+        border: `1px solid ${isDarkMode ? '#444' : '#ccc'}`,
         borderRadius: '8px',
         padding: '16px',
-        color: context.isDarkMode ? '#fff' : '#000',
-        backgroundColor: context.isDarkMode ? '#333' : '#fff',
-      }}
+        color: isDarkMode ? '#fff' : '#000',
+        backgroundColor: isDarkMode ? '#333' : '#fff',
+      }}  
     >
       <strong>{title}</strong>
       <div style={{ margin: 8 }}>
-        {content.map((item) => (
-          <ContentItem key={item.id} item={item} />
-        ))}
+        {isLoggedIn ? <> {content} </> : <>로그인 후 내용을 확인하세요.</>}
       </div>
     </div>
   );
-}
+} 
+          
 
 function App() {
-  const data = [
-    { id: 1, text: 'JSX 알아보기' },
-    { id: 2, text: '컴포넌트 스타일링 해보기' },
-    { id: 3, text: '일정 관리 앱 만들어 보기' },
-  ];
+  const [_, setIsStateChanged] = useState(false);
 
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setIsStateChanged((prev) => !prev);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
-    <MyContext value={{ isDarkMode }}>
-      <div>
-        <button onClick={() => setIsDarkMode(!isDarkMode)}>
-          Toggle Dark Mode : {isDarkMode ? 'ON' : 'OFF'}
-        </button>
-        <Card
-          title="오늘 공부 체크리스트"
-          content={data}
-        />
-      </div>
-    </MyContext>
+    <div>
+      <button onClick={() => store.dispatch({ type: 'DARK_MODE' })}>
+        Toggle Dark Mode : {store.getState().isDarkMode ? 'ON' : 'OFF'}
+      </button>
+      <button onClick={() => store.dispatch({ type: 'LOGGED_IN', payload: {isLoggedIn:!store.getState().isLoggedIn} })}>
+        {store.getState().isLoggedIn ? '로그아웃' : '로그인'}
+      </button>
+      <Card
+        title="오늘 공부 체크리스트"
+        content="리액트 Context API 사용하기"
+      />
+    </div>
   );
 }
 
