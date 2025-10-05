3d7360b2980fe17ace152008c7284e02622cb18d
the-eris-net <the.eris.net@gmail.com>
2025-10-06 08:46:13 +0900

    feat:finish 35p

--- 35p_redux_default_separate/src/reducers/index.js
+++ 35p_redux_default_separate/src/reducers/index.js
@@ -1,18 +1,14 @@
-import { createStore } from 'redux';
+import { DARK_MODE, LOGGED_IN } from "../actions";
 
 const initialState = { isDarkMode: false, isLoggedIn: true };
 
-function myReducer(state = initialState, action) {
+export default function myReducer(state = initialState, action) {
   switch (action.type) {
-    case 'DARK_MODE':
+    case DARK_MODE:
       return { ...state, isDarkMode: !state.isDarkMode };
-    case 'LOGGED_IN':
+    case LOGGED_IN:
       return { ...state, isLoggedIn: action.payload.isLoggedIn };
     default:
       return state;
   }
 }
-
-const store = createStore(myReducer);
-
-export default store;
--- 35p_redux_default_separate/src/stores/index.js
+++ 35p_redux_default_separate/src/stores/index.js
@@ -1,18 +1,6 @@
 import { createStore } from 'redux';
+import myReducer from '../reducers';
 
-const initialState = { isDarkMode: false, isLoggedIn: true };
-
-function counterReducer(state = initialState, action) {
-  switch (action.type) {
-    case 'DARK_MODE':
-      return { ...state, isDarkMode: !state.isDarkMode };
-    case 'LOGGED_IN':
-      return { ...state, isLoggedIn: action.payload.isLoggedIn };
-    default:
-      return state;
-  }
-}
-
-const store = createStore(counterReducer);
+const store = createStore(myReducer);
 
 export default store;
